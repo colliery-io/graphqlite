ed15d661712146970bc2c1a34ee3d49e8c9b0af6
Dylan Storey <dstorey@gmail.com>
2026-01-08 20:35:00 -0500

    chore: prep v0.2.1-rc.1 release
    - Update Rust publish workflow to use bundled binaries
    - Add include directive to Cargo.toml for libs/ directory
    - Add LICENSE file to Rust crate
    - Bump version to 0.2.1-rc.1

--- bindings/python/src/graphqlite/__init__.py
+++ bindings/python/src/graphqlite/__init__.py
@@ -8,7 +8,7 @@ from .manager import GraphManager, graphs
 from .utils import escape_string, sanitize_rel_type, CYPHER_RESERVED
 from ._platform import get_loadable_path
 
-__version__ = "0.2.0"
+__version__ = "0.2.1-rc.1"
 __all__ = [
     "Connection", "connect", "wrap", "load", "loadable_path",
     "Graph", "graph", "GraphManager", "graphs",
