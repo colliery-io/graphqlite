2276b8831b87d5cdcc4becf297cefc21912ddfbe
Dylan Bobby Storey <dstorey@dstorey-personal-m3.local>
2026-05-14 12:48:38 -0400

    feat(tck): register localdatetime/time/localtime.truncate (+converts 166 errors to fails)

--- src/backend/transform/transform_func_dispatch.c
+++ src/backend/transform/transform_func_dispatch.c
@@ -169,6 +169,12 @@ static const transform_func_entry dispatch_table[] = {
     {"dateTruncate",            transform_date_truncate_function},
     {"datetime.truncate",       transform_date_truncate_function},
     {"datetimeTruncate",        transform_date_truncate_function},
+    {"localdatetime.truncate",  transform_date_truncate_function},
+    {"localdatetimeTruncate",   transform_date_truncate_function},
+    {"time.truncate",           transform_date_truncate_function},
+    {"timeTruncate",            transform_date_truncate_function},
+    {"localtime.truncate",      transform_date_truncate_function},
+    {"localtimeTruncate",       transform_date_truncate_function},
 
     /* Duration utility functions */
     {"duration.between",        transform_duration_between_function},
